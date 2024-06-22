c0a6511bdff8375fc4b39d4769231c123cad6284
Andriel Abreu Rosa <diel132619@gmail.com>
2024-06-22 11:53:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+ganhoHora = float(input("Digite seu quanto você ganha por hora: "))
+horas = float(input("Digite as horas trabalhadas no mês: "))
+salario= ganhoHora * horas
+print(f"Seu salário será de :  {salario}")
